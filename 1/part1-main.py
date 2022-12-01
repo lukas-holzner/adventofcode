345500a2eee05220b8002f9976993d8bd3e0a595
Lukas Holzner <lukas@familie-holzner.de>
2022-12-01 23:37:30 +0100

    Day 1 - Part 1

---
+++ 1/part1-main.py
@@ -0,0 +1,20 @@
+with open('1/part1-input.txt') as f:
+    lines = f.readlines()
+
+elves = [0]
+
+for l in lines:
+    line = l.rstrip('\n')
+    if line == '':
+        elves.append(0)
+    else:
+        elves[len(elves)-1] += int(line)
+
+bestelv = (-1,0)
+for idx,elv in enumerate(elves):
+    print("Elv {} with {} calories".format(idx+1, elv))
+    if elv > bestelv[1]:
+        bestelv = (idx, elv)
+
+print("The best Elv is : \n\n")
+print("Elv {} with {} calories".format(bestelv[0]+1, bestelv[1]))
