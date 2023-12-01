3a3ae488a5905d3763d934668c8aa59471a94e99
Lukas Holzner <holzner@fs.tum.de>
2023-12-01 23:25:48 +0000

    Added part2 of day 1

--- 2023/1/part2.py
+++ 2023/1/part2.py
@@ -1,21 +1,32 @@
 #!/usr/bin/python
 totalsum = 0
 
+def reversestring(word: str):
+    return word[::-1]
+
+def getfirstnumber(written, line):
+    word=""
+    for ch in line:
+        if ch in "123456789":
+            return ch
+        word += ch
+        stoploop = False
+        for idx, item in enumerate(written):
+            if item in word:
+                return str(idx+1)
+    raise Exception
+
+writtennumbers= ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine" ]
+writtennumbersreverse = list(map(reversestring, writtennumbers))
+
 with open('2023/1/input.txt') as f:
     lines = f.readlines()
 
 for line in lines:
     line = line.rstrip('\n')
     number=""
-    for ch in line:
-        if ch in "0123456789":
-            number += ch
-            break
-    
-    for ch in line[::-1]:
-        if ch in "0123456789":
-            number += ch
-            break
+    number += str(getfirstnumber(writtennumbers, line))
+    number += str(getfirstnumber(writtennumbersreverse, reversestring(line)))   
     
     print(number)
     totalsum += int(number)
